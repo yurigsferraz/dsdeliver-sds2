3eb8fbf065e00befa37b30d7cf97b51fec9e6129
yferraz <62407635+yferraz@users.noreply.github.com>
2021-01-10 21:11:35 -0300

    Selected products in orders

--- front-web/src/Orders/ProductCard.tsx
+++ front-web/src/Orders/ProductCard.tsx
@@ -2,6 +2,8 @@ import { Product } from './types';
 
 type Props = {
     product: Product;
+    onSelectProduct: (product: Product) => void;
+    isSelected: boolean;
 }
 
 function formatPrice(price: number) {
@@ -13,9 +15,12 @@ function formatPrice(price: number) {
     return formatter.format(price);
 ;}
 
-function ProductCard({ product }: Props){
+function ProductCard({ product, onSelectProduct, isSelected }: Props){
     return(
-        <div className="order-card-container">
+        <div
+        className={`order-card-container ${isSelected ? 'selected' : ''}  `}
+        onClick={() => onSelectProduct(product)}
+        >
             <h3 className="order-card-title">
                 {product.name}
             </h3>
--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,27 +1,56 @@
 import { useEffect, useState } from 'react';
+import { SelectContainer } from 'react-select/src/components/containers';
 import { fetchProducts } from '../api';
+import Footer from '../Footer';
+import { checkIsSelected } from './helpers';
 import OrderLocation from './OrderLocation';
+import OrderSummary from './OrderSummary';
 import ProductsList from './ProductsList';
 import StepsHeader from './StepsHeader';
 import './styles.css';
 import { OrderLocationData, Product } from './types';
 
-function Orders(){
+function Orders() {
     const [products, setProducts] = useState<Product[]>([]);
+    const [selectedProducts, setSelectedProducts] = useState<Product[]>([]);
     const [orderLocation, setOrderLocation] = useState<OrderLocationData>();
+    const totalPrice = selectedProducts.reduce((sum, item) => {
+        return sum + item.price;
+    }, 0);
 
-    useEffect(() =>{
+    useEffect(() => {
         fetchProducts()
             .then(response => setProducts(response.data))
             .catch(error => console.log(error))
     }, [])
 
-    return(
-        <div className="orders-container">
-            <StepsHeader />
-            <ProductsList products={products} />
-            <OrderLocation onChangeLocation={location => setOrderLocation(location)}/>
-        </div>
+    const handleSelectProduct = (product: Product) => {
+        const isAlreadySelected = checkIsSelected(selectedProducts, product);
+
+        if (isAlreadySelected) {
+            const selected = selectedProducts.filter(item => item.id !== product.id);
+            setSelectedProducts(selected);
+        } else {
+            setSelectedProducts(previous => [...previous, product]);
+        }
+    }
+
+    return (
+        <>
+            <div className="orders-container">
+                <StepsHeader />
+                <ProductsList
+                    products={products}
+                    onSelectProduct={handleSelectProduct}
+                    selectedProducts={selectedProducts}
+                />
+                <OrderLocation
+                    onChangeLocation={location => setOrderLocation(location)}
+                />
+                <OrderSummary />
+            </div>
+            <Footer />
+        </>
     )
 }
 
